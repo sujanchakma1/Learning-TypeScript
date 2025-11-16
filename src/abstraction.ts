24adaff7b1bc53ffffff3c48b0357b5659e97f82
sujanchakma1 <sujanckz926@gmail.com>
2025-11-16 19:24:21 +0600

    added Abstraction three pillar of OOP

---
+++ src/abstraction.ts
@@ -0,0 +1,42 @@
+// * Using interface
+interface Sound{
+  start():void
+  stop(): void
+}
+
+class MySound implements Sound{
+  start(): void {
+      console.log("Sound Started.......");
+  }
+  stop(): void {
+      console.log("Sound Stopped........");
+  }
+}
+const MySound1 = new MySound()
+MySound1.start()
+
+
+// * Using Abstract Class
+
+abstract class Vehicle {
+  abstract start(vehicle:string): void
+  abstract running(vehicle:string): void
+  abstract stop(vehicle:string): void
+}
+
+class Car extends Vehicle{
+  start(vehicle: string): void {
+      console.log(`${vehicle} Engine is Started.......`);
+  }
+  running(vehicle:string): void {
+      console.log(`${vehicle} is Running......`);
+  }
+  stop(vehicle:string): void {
+      console.log(`${vehicle} is Stopped.........`);
+  }
+}
+
+const bike = new Car()
+bike.running('bike')
+bike.start("BMW")
+
